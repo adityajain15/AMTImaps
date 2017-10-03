0152cc8b5abe2749e5a02d84599734edf99da8e5
Aditya J <ajain15@uic.edu>
2017-10-03 18:13:40 -0400

    style changes

--- interactive.js
+++ interactive.js
@@ -8,110 +8,54 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzd
 const layerTypes = ["Exclusive Economic Zone","Territorial Sea","Continental Shelf","Territorial Baseline"];
 
 d3.selectAll(".option").on("change",function changeEventHandler(event){
-    /*var visibility = map.getLayoutProperty(this.id, 'visibility');
-    if(visibility === 'visible'){
-      map.setLayoutProperty(this.id, 'visibility', 'none');
-    }
-    else{
-      map.setLayoutProperty(this.id, 'visibility', 'visible');
-    }*/
-    //console.log(this.id);
-    var selectedCountry = this.id;
-    map.getStyle().layers.map(function(each){
-      if(each["source-layer"]==="all_claims-7jhily"&&each["id"]===selectedCountry){
-        if(map.getLayoutProperty(each['id'], 'visibility')==='visible'){
-          map.setLayoutProperty(each['id'], 'visibility', 'none');
-          layerTypes.map(function(layer){
-            var filter = map.getFilter(layer);
-            filter[1].push(selectedCountry);
-            map.setFilter(layer,filter);
-          });
-        }
-        else{
-          map.setLayoutProperty(each['id'], 'visibility', 'visible');
-          layerTypes.map(function(layer){
-            var filter = map.getFilter(layer);
-            filter[1].splice(filter[1].indexOf(selectedCountry),1);
-            map.setFilter(layer,filter);
-          });
-        }
-        
+  var selectedCountry = this.id;
+  toggle(selectedCountry);
+});
 
-      }
-    });
-  
+function toggle(selectedCountry){
+  map.getStyle().layers.map(function(each){
+    if(each["source-layer"]==="all_claims-7jhily"&&each["id"]===selectedCountry){
 
-  
-  
-      
-  });
+      if(map.getLayoutProperty(each['id'], 'visibility')==='visible'){
+        map.setLayoutProperty(each['id'], 'visibility', 'none');
+        layerTypes.map(function(layer){
 
-  /*
-d3.selectAll(".option").on("change",function changeEventHandler(event){
-    var visibility = map.getLayoutProperty(this.id, 'visibility');
-    if(visibility === 'visible'){
-      map.setLayoutProperty(this.id, 'visibility', 'none');
-    }
-    else{
-      map.setLayoutProperty(this.id, 'visibility', 'visible');
-    }
-  });
+          var filter = map.getFilter(layer);
+          if(filter.length!=2){
+            var newFilter = [].push(filter);
+            newFilter = newFilter.push(["!in","country"]);
+            map.setFilter(layer,newFilter);
+          }
+          filter[1].push(selectedCountry);
+          map.setFilter(layer,filter);
 
-
-  
-
-
-  var types = ["Exclusive Economic Zone","Continental Shelf","Territorial Baseline","Territorial Sea"];
-  var sources = ["singapore","china","japan"];
-
-  map.on('load', function() {
-    map.addSource("singapore",  {
-        type: "geojson",
-        data: "singapore_claims_1.geojson"
-    });
-    map.addSource("japan",  {
-        type: "geojson",
-        data: "japan_claims_1.geojson"
-    });
-    map.addSource("china",  {
-        type: "geojson",
-        data: "china_claims_1.geojson"
-    });
-
-
-      for(var j=0;j<sources.length;j++){
-        
-        map.addLayer({
-        "id": sources[j],
-        "type": "line",
-        "source": sources[j],
-        "layout": {
-          "line-join": "round",
-          "line-cap": "round",
-          "visibility": "visible",
-        },
-        "paint": {
-          "line-color": "lightgreen",
-          "line-width": 4
-        }
-        });
-      
-        map.addLayer({
-        "id": "symbols"+sources[j],
-        "type": "symbol",
-        "source": sources[j],
-        "minzoom": 1,
-        "layout": {
-          "symbol-placement": "line",
-          "text-font": ["Open Sans Regular"],
-          "text-field": '{name}', 
-          "text-size": 14
-        },
-        "paint":{
-          "text-color":"white"
-        }
         });
       }
-    
+      else{
+        map.setLayoutProperty(each['id'], 'visibility', 'visible');
+        layerTypes.map(function(layer){
+          
+          var filter = map.getFilter(layer);
+          if(filter.length!=2){
+            var newFilter = [].push(filter);
+            newFilter.push(["!in","country"]);
+            map.setFilter(layer,newFilter);
+          }
+          filter[1].splice(filter[1].indexOf(selectedCountry),1);
+          map.setFilter(layer,filter);
+        
+        });
+      }
+    }
+  });
+}
+map.on('style.load', function(){
+  Array.prototype.map.call(document.getElementsByClassName('option'), 
+  function(option){
+    if(!option.checked){
+      var selectedCountry = option.id;
+      toggle(selectedCountry);
+    }
+  });
+});
 
-  */
